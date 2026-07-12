ff615a4155664e1876dc11e77e7b10c763c59b45
venomouus <gustavonascimento62.dev@gmail.com>
2026-07-12 17:50:18 -0300

    Add Kubernetes manifests and API health check

--- Oficina.API/Program.cs
+++ Oficina.API/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks();
 builder.Services.AddScoped<ClienteService>();
 builder.Services.AddScoped<VeiculoService>();
 builder.Services.AddScoped<ServicoService>();
@@ -92,7 +93,7 @@ if (!app.Environment.IsEnvironment("Testing"))
 }
 
 
-app.UseHttpsRedirection();
+app.MapHealthChecks("/health");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
@@ -101,3 +102,5 @@ app.Run();
 public partial class Program
 {
 }
+
+
